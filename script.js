bb38b546f6f7d52d9d5b4b5e66ba872e4fac7089
VeeraMahendra7 <140475884+VeeraMahendra7@users.noreply.github.com>
2025-09-29 12:18:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	arr.sort((a,b)=>a-b);
+	let sum = 0;
+	const a = arr.map((ele,ind)=>{
+		sum += ele;
+		return sum;
+	})
+	let mnCost = 0;
+	for(let i=0;i<a.length;i++){
+		mnCost += a[i];
+	}
+	return mnCost;
 }
 
 module.exports=mincost;
